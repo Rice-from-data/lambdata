e7ebd1be233e2f1cd8c4c8adabaa0a6bb61ef66a
Rice_from_data <nedderlander@gmail.com>
2019-05-28 06:56:53 -0700

    updated

--- lambdata_rice_from_data/__init__.py
+++ lambdata_rice_from_data/__init__.py
@@ -3,9 +3,4 @@
 lambdata - a collection of Data Science helper functions
 """
 
-import pandas as pd
-import numpy as np
 from . import example_module
-
-Y = example_module.increment(example_module.x)
-TEST = pd.DataFrame(np.ones(10))
--- lambdata_rice_from_data/example_module.py
+++ lambdata_rice_from_data/example_module.py
@@ -6,5 +6,13 @@ lambdata - a collection of Data Science helper functions
 import pandas as pd
 import numpy as np
 
-TEST = pd.Dataframe(np.ones(10))
+
+def clean_column_names(df):
+    df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_').str.replace('(', '').str.replace(')', '').str.replace('/', '')
+    return df
+
+def squared_feature(df,list):
+    for feature in list:
+        df[feature + '_squared'] = df[feature] ** 2
+    return df
 
